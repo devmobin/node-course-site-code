b5e9984c4e2751bd14b5c7e3b8ebc63716e6c0e3
Mobin Heydari <mobinheydari114@gmail.com>
2019-09-04 17:59:29 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const MONGODB_USER = "devmobin";
-const MONGODB_PASSWORD = "P5okQo3OVbBydkj5";
+const MONGODB_USER = "";
+const MONGODB_PASSWORD = "";
 
-exports.MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@firstcluster-iqj0g.mongodb.net/node_shop?retryWrites=true&w=majority`;
+exports.MONGODB_URI = ``;
 
 exports.PORT = 3000;
